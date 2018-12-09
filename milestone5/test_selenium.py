d52c731ff2c294c63bc5a612ea12bdac09af2399
achamorr <andrea.colleges@gmail.com>
2018-12-08 19:39:09 -0700

    milestone 5 selenium tests added, old tests in old_tests branch

---
+++ milestone5/test_selenium.py
@@ -0,0 +1,40 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+
+class BasicSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+
+    def test_button_dropdown(self):
+        driver = self.driver
+        driver.get("http://10.201.64.35/cgi-bin/webgui2.py")
+        #checl that title is corrent
+        self.assertIn("Raspberry Pi Temperature Logger", driver.title)
+        #Test that each of the time intervals work
+        select = Select(driver.find_element_by_name("timeinterval"))
+        select.select_by_visible_text("the last 6 hours")
+
+
+        select = Select(driver.find_element_by_name("timeinterval"))
+        select.select_by_visible_text("the last 12 hours")
+
+
+        select = Select(driver.find_element_by_name("timeinterval"))
+        select.select_by_visible_text("the last 24 hours")
+
+        #Test that the update button works
+        select = driver.find_element_by_xpath("//input[@value='Display']").click()
+
+        #Test that 
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ milestone5/writeup.md
@@ -0,0 +1,15 @@
+We used the following for our unit-tests:
+
+* Selenium- Used for web browser automation
+
+We checked that it worked on both Firefox and Chrome
+and that user inputs such as a dropdown menu and a submit buttom worked as expected. 
+
+This can only be run if the Raspberry Pi is running and connected to the same router as the pi.
+Point browser to http://10.201.64.35/cgi-bin/webgui2.py to get webapge. 
+
+
+Resources:
+https://www.techbeamers.com/selenium-webdriver-python-tutorial/
+https://selenium-python.readthedocs.io/
+https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.chrome.webdriver
